b048e680a79462b980b97ad8400abb0548f56947
Aleksey Stryukov <aleksey.stryukov@gmail.com>
2018-09-07 17:12:01 +0300

    add mainProcurementCategory to the base tender model

--- openprocurement/tender/core/models.py
+++ openprocurement/tender/core/models.py
@@ -786,6 +786,7 @@ class BaseTender(SchematicsDocument, Model):
     if SANDBOX_MODE:
         procurementMethodDetails = StringType()
     funders = ListType(ModelType(Organization), validators=[validate_funders_unique, validate_funders_ids])
+    mainProcurementCategory = StringType(choices=["goods", "services", "works"])
 
     _attachments = DictType(DictType(BaseType), default=dict())  # couchdb attachments
     revisions = ListType(ModelType(Revision), default=list())
